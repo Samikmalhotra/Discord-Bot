1f5eae5c9cb473cd0d9a803b137059c3398a9710
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-29 21:28:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,17 +23,40 @@ client.on('message', message => {
         }
     
         message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+     }else if (command === 'kick') {
+        if (!message.mentions.users.size) {
+            return message.reply('you need to tag a user in order to kick them!');
         }
-    if (message.content === prefix+'ping') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
-    }else if (message.content === `${prefix}beep`) {
-        message.channel.send('Boop.');
-    }else if (message.content === `${prefix}server`) {
-        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    }else if (message.content === `${prefix}user-info`) {
-        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+        // grab the "first" mentioned user from the message
+        // this will return a `User` object, just like `message.author`
+        const taggedUser = message.mentions.users.first();
+    
+        message.channel.send(`You wanted to kick: ${taggedUser.username}`);
+    }else if (command === 'avatar') {
+        if (!message.mentions.users.size) {
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+        }
+    
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+        });
+    
+        // send the entire array of strings as a message
+        // by default, discord.js will `.join()` the array with `\n`
+        message.channel.send(avatarList);
     }
+
+    
+    // if (message.content === prefix+'ping') {
+    //     // send back "Pong." to the channel the message was sent in
+    //     message.channel.send('Pong.');
+    // }else if (message.content === `${prefix}beep`) {
+    //     message.channel.send('Boop.');
+    // }else if (message.content === `${prefix}server`) {
+    //     message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    // }else if (message.content === `${prefix}user-info`) {
+    //     message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    // }
 });
 
 client.login(token);
