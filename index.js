02d905a71adfa7ebbf59ad4d4f88a46284290eb2
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-29 20:21:54 +0530

    updated commands : ping, beep, server

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const dotenv = require('dotenv');
-dotenv.config();
+const discord = require('discord.js');
+const client = new discord.Client();
+const {prefix, token} = require('./config.json');
 
-console.log(process.env.Token)
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	console.log(message.content);
+    if (message.content === prefix+'ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }else if (message.content === `${prefix}beep`) {
+        message.channel.send('Boop.');
+    }else if (message.content === `${prefix}server`) {
+        message.channel.send(`This server's name is: ${message.guild.name}`);
+    }
+});
+
+client.login(token);
+
+
