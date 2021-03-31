6276124ff854611773e9d888cbdcc5d323bca6b9
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-04-01 02:28:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -101,5 +101,9 @@ client.on('shardError', error => {
 	console.error('A websocket connection encountered an error:', error);
 });
 
+process.on('unhandledRejection', error => {
+	console.error('Unhandled promise rejection:', error);
+});
+
 // Login 
 client.login(token);
