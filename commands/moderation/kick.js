6e006c44622f7fb8547dbc5c54988d19b3bf41f4
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-03-31 13:55:48 +0530

    added cooldown

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'kick',
 	description: 'Kick!',
+    guildOnly: true,
 	execute(message, args) {
         if (!message.mentions.users.size) {
             return message.reply('you need to tag a user in order to kick them!');
--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
 
+const { cooldowns } = client;
+
+// get commands from commandFolders
 const commandFolders = fs.readdirSync('./commands');
 for (const folder of commandFolders) {
 	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
@@ -29,9 +33,26 @@ client.on('message', message => {
 
 	const command = client.commands.get(commandName);
 
+
+	// Cooldown
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	// Guild only
+	if (command.guildOnly && message.channel.type === 'dm') {
+		return message.reply('I can\'t execute that command inside DMs!');
+	}
+
+	// Args needed
 	if (command.args && !args.length) {
 	    return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
 	}
+
+
 	try {
 		command.execute(message, args);
 	} catch (error) {
