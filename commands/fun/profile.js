89ccb7bd83d549efccd77d5376401fc8803c9f61
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-04-01 02:26:00 +0530

    Update profile.js

--- commands/fun/profile.js
+++ commands/fun/profile.js
@@ -8,20 +8,12 @@ module.exports = {
             .setColor('#0099ff')
             .setTitle(message.author.username)
             .setURL(message.author.displayAvatarURL({ format: "png", dynamic: true }))
-            .setAuthor('Bot', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-            .setDescription(message.author.role.name)
+            .setAuthor('Sataniel: ')
+            .setDescription(message.guild.region)
             .setThumbnail(message.author.displayAvatarURL({ format: "png", dynamic: true }))
-            .addFields(
-                { name: 'Regular field title', value: 'Some value here' },
-                { name: '\u200B', value: '\u200B' },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-                { name: 'Inline field title', value: 'Some value here', inline: true },
-            )
-            .addField('Inline field title', 'Some value here', true)
-            .setImage('https://i.imgur.com/wSTFkRM.png')
             .setTimestamp()
-            .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
+            .setFooter(message.guild, 'https://i.imgur.com/wSTFkRM.png');
         message.channel.send(exampleEmbed);
-	},
+        
+    },
 };
