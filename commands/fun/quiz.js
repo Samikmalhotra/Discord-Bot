f9d929f80b6b79b5891839cff9880d1322dd312f
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-04-01 15:55:29 +0530

    added quiz

---
+++ commands/fun/quiz.js
@@ -0,0 +1,23 @@
+
+
+module.exports = {
+	name: 'quiz',
+	description: 'Play quiz',
+	execute(message, args, client) {
+
+        const quiz = require('../../quiz.json');
+        const item = quiz[Math.floor(Math.random() * quiz.length)];
+        const filter = response => {
+            return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
+        };
+		message.channel.send(item.question).then(() => {
+            message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+                .then(collected => {
+                    message.channel.send(`${collected.first().author} got the correct answer!`);
+                })
+                .catch(collected => {
+                    message.channel.send('Looks like nobody got the answer this time.');
+                });
+        });
+	},
+};
