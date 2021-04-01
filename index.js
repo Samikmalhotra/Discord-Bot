c1e530b5ad55b65db4b7a1457f83f53c525b80b6
Samikmalhotra <72279316+Samikmalhotra@users.noreply.github.com>
2021-04-02 01:38:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,20 @@ const sequelize = new Sequelize('database', 'user', 'password', {
 	storage: 'database.sqlite',
 });	
 
+const Tags = sequelize.define('tags', {
+	name: {
+		type: Sequelize.STRING,
+		unique: true,
+	},
+	description: Sequelize.TEXT,
+	username: Sequelize.STRING,
+	usage_count: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0,
+		allowNull: false,
+	},
+});
+
 // get events
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 for (const file of eventFiles) {
